64c9cabda2017bb7b81f6875b61d07dd651a9e7b
Digvijay <digvijay@logisticinfotech.co.in>
2020-09-10 18:15:50 +0530

    remove UIWebView code.

--- src/ios/GeofencePlugin.swift
+++ src/ios/GeofencePlugin.swift
@@ -168,11 +168,8 @@ func log(_ messages: [String]) {
 
     func evaluateJs (_ script: String) {
         if let webView = webView {
-            if let uiWebView = webView as? UIWebView {
-                uiWebView.stringByEvaluatingJavaScript(from: script)
-            } else if let wkWebView = webView as? WKWebView {
-                wkWebView.evaluateJavaScript(script, completionHandler: nil)
-            }
+            let wkWebView = webView as? WKWebView
+            wkWebView?.evaluateJavaScript(script, completionHandler: nil);
         } else {
             log("webView is nil")
         }
